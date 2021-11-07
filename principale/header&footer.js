6eaa931a31f47fedf7e974ba98d7bbca2116affe
12dorian12 <45594474+12dorian12@users.noreply.github.com>
2021-11-08 00:01:28 +0100

    Update header&footer.js

--- principale/header&footer.js
+++ principale/header&footer.js
@@ -7,7 +7,7 @@
 /*################################## definition des variable du local storage ########################################*/
 
 if (typeof(localStorage["user"]) == "undefined"){
-    fetch("https://12dorian12.github.io/Projet_TLW//json/people.json")
+    fetch("https://12dorian12.github.io/Projet_TLW/json/people.json")
     .then((response) => response.json())
     .then((perso)=>{
         localStorage["user"]=JSON.stringify(perso.user);
@@ -39,7 +39,7 @@ if (typeof(localStorage["infoClient"]) == "undefined"){
 /*################################## XMLHttpRequest ########################################*/
 
 
-fetch("https://12dorian12.github.io/Projet_TLW//principale/header.html") //fetch renvoie une promesse resolue avec un objet response 
+fetch("https://12dorian12.github.io/Projet_TLW/principale/header.html") //fetch renvoie une promesse resolue avec un objet response 
 .then(response => response.text()) //response.text renvoie une promesse resolue avec un USVString (qui contient le text du stream de response)
 .then(body => {
     document.querySelector('header').innerHTML = body;
@@ -47,7 +47,7 @@ fetch("https://12dorian12.github.io/Projet_TLW//principale/header.html") //fetch
     var user = JSON.parse(localStorage["user"]);
     if (localStorage["etatConnexion"] != -1){
         document.querySelector('#titreConnexion').innerHTML = user[localStorage["etatConnexion"]].prenom;
-        document.querySelector('#lienConnexion').href = "https://12dorian12.github.io/Projet_TLW//seConnecter/connected/connected.html";
+        document.querySelector('#lienConnexion').href = "https://12dorian12.github.io/Projet_TLW/seConnecter/connected/connected.html";
     }
     else{
         document.querySelector('#titreConnexion').innerHTML = "Se Connecter";
@@ -55,7 +55,7 @@ fetch("https://12dorian12.github.io/Projet_TLW//principale/header.html") //fetch
    
 });
 
-fetch("https://12dorian12.github.io/Projet_TLW//principale/footer.html")
+fetch("https://12dorian12.github.io/Projet_TLW/principale/footer.html")
 .then(response => response.text())
 .then(body => {
     document.querySelector('footer').innerHTML = body;
